980ce8665f034d7aae8d0c5ae8eedadd8d9e9ff5
Vojin Puric <puricvojin@gmail.com>
2022-08-22 17:30:54 +0200

    install offline software and extract portable software

--- bin/initializeOS.sh
+++ bin/initializeOS.sh
@@ -6,6 +6,10 @@ sudo apt upgrade
 # gdebi for installing packages
 sudo apt install -yy gdebi
 
+### move scripts to bin ###
+echo "Moving scripts to /usr/local/bin"
+sudo cp -a ./bin/. /usr/local/bin/
+
 ### Setup Git ###
 echo "### Setting up git ###"
 git config --global user.name "Vojin Puric"
@@ -21,4 +25,32 @@ sudo printf "export PATH=\$JAVA_HOME/bin:\$PATH\n" | tee -a /etc/profile
 sudo source /etc/profile
 
 # TODO ### Setup fish ###
+
+### Run offline installers ###
+echo "### Running Offline Installers ###"
+if [ -d "./installers" ]; 
+then
+    for file in ./installers/*
+    do
+        echo "### Installing $file"
+        sudo dpkg -i "$file"
+    done
+fi
+
+### Extracting portable software ###
+if [ -d "./portable" ]; 
+then
+    for file in ./portable/*.tar
+    do
+        echo "### Extracting $file to HOME"
+        tar xvf $file -C ~ || echo "extracting $file failed" > /dev/stderr
+    done
+    for file in ./portable/*.tar.gz
+    do
+        echo "### Extracting $file to HOME"
+        ar xvzf $file -C ~ || echo "extracting $file failed" > /dev/stderr
+    done
+    
+fi
+
 # TODO ### Setup VS Code ###
