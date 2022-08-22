fbba0d55b46ade3904471a5ec4c3592c9fc283e4
Vojin Puric <puricvojin@gmail.com>
2022-08-22 15:29:19 +0200

    General update

--- bin/initializeOS.sh
+++ bin/initializeOS.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+### General ###
+sudo apt update
+sudo apt upgrade
+# gdebi for installing packages
+sudo apt install -yy gdebi
+
 ### Setup Git ###
 echo "### Setting up git ###"
 git config --global user.name "Vojin Puric"
@@ -8,7 +14,11 @@ git config --list --global
 
 ### Setup JDK ###
 echo "### Setting up JDK ###"
-apt install openjdk-11-jdk
+sudo apt install --yy openjdk-11-jdk
 JAVA_HOME_PATH=$(dirname $(dirname $(readlink -f $(which javac)))) || echo "Couldn't find Java"
-printf "\nexport JAVA_HOME=\"%s\"\n" $JAVA_HOME_PATH | tee -a /etc/profile
-printf "export PATH=\$JAVA_HOME/bin:\$PATH\n" | tee -a /etc/profile
+sudo printf "\nexport JAVA_HOME=\"%s\"\n" $JAVA_HOME_PATH | tee -a /etc/profile
+sudo printf "export PATH=\$JAVA_HOME/bin:\$PATH\n" | tee -a /etc/profile
+sudo source /etc/profile
+
+# TODO ### Setup fish ###
+# TODO ### Setup VS Code ###
