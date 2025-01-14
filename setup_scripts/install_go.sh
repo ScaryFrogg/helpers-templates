801c2426d6af76fe9f0816159ba9566041199e08
Vojin <puricvojin@gmail.com>
2025-01-14 16:20:58 +0100

    if nvim installed use as default git editor

--- setup_scripts/install_go.sh
+++ setup_scripts/install_go.sh
@@ -2,6 +2,6 @@
 
 curl -OL https://go.dev/dl/go1.23.1.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.23.1.linux-amd64.tar.gz
-sudo printf "export PATH=$PATH:/usr/local/go/bin\n" >> ~/.bashrc
+sudo printf "export PATH=\$PATH:/usr/local/go/bin\n" >> ~/.bashrc
 sudo rm go1.23.1.linux-amd64.tar.gz
-go version
+git config --global core.editor "nvim -f"
